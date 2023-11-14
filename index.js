aff3884228e176b42bfa0279dc01f3587ab06ab1
iamapusaha <apusaha392@gmail.com>
2023-11-15 03:40:26 +0600

    get all reviews data

--- index.js
+++ index.js
@@ -26,8 +26,13 @@ async function run() {
 
         // Send a ping to confirm a successful connection
         const menuCollection = client.db("restaurantDB").collection("menu");
+        const reviewCollection = client.db("restaurantDB").collection("reviews");
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray()
             res.send(result)
         })
 
