db4aa28b1efbc14b3b12a45ddf4c9ef670ea7a76
iamapusaha <apusaha392@gmail.com>
2023-11-19 14:26:47 +0600

    get carts data by email query

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ async function run() {
         })
         //cart related api
         app.get('/carts', async (req, res) => {
-            const result = await cartCollection.find().toArray();
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
             res.send(result)
         })
         app.post('/carts', async (req, res) => {
