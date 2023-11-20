93d2779f4bd41c0d5d8b32dd1866d7d5c3f8a6ab
iamapusaha <apusaha392@gmail.com>
2023-11-20 16:26:56 +0600

    added delete cart item by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -49,6 +49,12 @@ async function run() {
         app.post('/carts', async (req, res) => {
             const cartInfo = req.body;
             const result = await cartCollection.insertOne(cartInfo);
+            res.send(result)
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
 
