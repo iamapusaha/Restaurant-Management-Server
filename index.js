03f9b92616bd6ec40912f05e7b0419e143da296a
iamapusaha <apusaha392@gmail.com>
2023-11-21 20:54:56 +0600

    delete user data by id

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ async function run() {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // All menu related api
         app.get('/menu', async (req, res) => {
