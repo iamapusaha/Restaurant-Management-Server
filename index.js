c55902c5a862c78dea43c9fb4027dd0cc111f938
iamapusaha <apusaha392@gmail.com>
2023-11-21 23:49:08 +0600

    added verifyToken middleware

--- index.js
+++ index.js
@@ -38,6 +38,21 @@ async function run() {
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ token })
         })
+        //middlewares
+        const verifyToken = (req, res, next) => {
+            console.log(req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized  access" })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    res.status(401).send({ message: "unauthorized  access" })
+                }
+                res.user = decoded;
+                next()
+            })
+        }
         // user related api
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
@@ -60,7 +75,8 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result)
         })
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
+
             const result = await userCollection.find().toArray();
             res.send(result)
         })
